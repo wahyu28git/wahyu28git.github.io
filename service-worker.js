64ac4c9ddd5afaf7e02544e7277cde1819c75ac5
wahyu28git <wahyu.24568@gmail.com>
2020-11-01 18:07:13 +0700

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,111 @@
+const CACHE_NAME = "bolaku2";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/manifest.json",
+  "/EPL-Logo512.png",
+  "/EPL-Logo192.png",
+  "/favicon.ico",
+  "/pages/standing.html",
+  "/pages/teams.html",
+  "/pages/favorit.html",
+  "/pages/about.html",
+  "/detail.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/js/nav.js",
+  "/js/api.js",
+  "/js/idb.js",
+  "/js/req.js"
+];
+
+workbox.routing.registerRoute(
+  new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages'
+    })
+);
+
+workbox.precaching.precacheAndRoute([
+  { url: '/index.html', revision: '1' },
+  { url: '/nav.html', revision: '1' },
+  { url: '/css/materialize.min.css', revision: '1' },
+  { url: '/js/materialize.min.js', revision: '1' },
+  { url: '/js/req.js', revision: '1' },
+]);
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+ 
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
+
+self.addEventListener('push', function(event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = 'Push message no payload';
+  }
+  var options = {
+    body: body,
+    icon: 'img/notification.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  var base_url = "https://api.football-data.org/v2/";
+  var onLine = navigator.onLine;
+
+  if (event.request.url.indexOf(base_url) > -1 && onLine) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        })
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+        return response || fetch (event.request);
+      })
+    )
+  }
+});
+
+self.addEventListener("activate", function(event) {
+  clients.claim();
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
