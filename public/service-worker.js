37b3decfade194fed9b146fbed6190234e1f5d8e
wahyu28git <wahyu.24568@gmail.com>
2020-11-04 20:37:29 +0700

    BolakuSub2

---
+++ public/service-worker.js
@@ -0,0 +1,64 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+const CACHE_NAME = "bolaku1";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/manifest.json",
+  "/EPL-Logo512.png",
+  "/EPL-Logo192.png",
+  "/favicon.ico",
+  "/pages/standing.html",
+  "/pages/teams.html",
+  "/pages/favorit.html",
+  "/pages/about.html",
+  "/detail.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/js/nav.js",
+  "/js/api.js",
+  "/js/idb.js",
+  "/js/req.js"
+];
+
+workbox.routing.registerRoute(
+  new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages'
+    })
+);
+
+workbox.precaching.precacheAndRoute([
+  { url: '/index.html', revision: '1' },
+  { url: '/nav.html', revision: '1' },
+  { url: '/css/materialize.min.css', revision: '1' },
+  { url: '/js/materialize.min.js', revision: '1' },
+  { url: '/js/req.js', revision: '1' },
+]);
+
+ 
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
+
+self.addEventListener('push', function(event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = 'Push message no payload';
+  }
+  var options = {
+    body: body,
+    icon: 'img/notification.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
